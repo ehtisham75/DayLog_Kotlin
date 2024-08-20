4426a41f8d8c1e1ddf002e21993f76239520691d
ehtisham75 <ehtishamofficial01@gmail.com>
2024-08-20 22:54:42 +0500

    remove bottom tab & toolbar

--- app/src/main/java/com/example/daylog/ui/passcode/PasscodeActivity.kt
+++ app/src/main/java/com/example/daylog/ui/passcode/PasscodeActivity.kt
@@ -1,10 +1,15 @@
 package com.example.daylog.ui.passcode
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet.Layout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.daylog.MainActivity
 import com.example.daylog.R
 
 class PasscodeActivity : AppCompatActivity() {
@@ -12,6 +17,13 @@ class PasscodeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_passcode)
+
+        val setPassCodeButton = findViewById<LinearLayout>(R.id.btn_set_passcode)
+        setPassCodeButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
